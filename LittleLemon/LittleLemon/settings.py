dddae95251788a25c7d1ce6b1483a3e7034ee25f
enrique-dominguez <enriquejosedominguez@gmail.com>
2023-11-14 20:11:02 -0400

    added static files as required

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Toolkits and Packages
+    # Custom APPs
+    'RestaurantAPP',
 ]
 
 MIDDLEWARE = [
