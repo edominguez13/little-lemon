558368f645c73d07f8ca99a81ccd9aab234baafa
enrique-dominguez <enriquejosedominguez@gmail.com>
2023-11-16 14:13:24 -0400

    Installed djangorestframework, set in settings.py and created serializers to Menu and Booking models

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Toolkits and Packages
+    'rest_framework',
     # Custom APPs
     'RestaurantAPP',
 ]
--- LittleLemon/RestaurantAPP/views.py
+++ LittleLemon/RestaurantAPP/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 
-from django.http import HttpResponse
+
 
 def sayHello(request):
     return HttpResponse('Hello World')
